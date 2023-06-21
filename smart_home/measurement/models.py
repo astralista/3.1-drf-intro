0dee3aa39b2cc2da1e161f71eb4960bb9611d7c9
Godz Dmitriy <woke@mail.ru>
2023-06-21 17:55:04 +0300

    in progress

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Weapon(models.Model):
+    power = models.IntegerField()
+    rarity = models.CharField(max_length=50)
+    value = models.IntegerField()
--- smart_home/measurement/views.py
+++ smart_home/measurement/views.py
@@ -1,2 +1,10 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+@api_view(['GET'])
+def demo(request):
+    data = {'message': 'Hello'}
+    return Response(data)
